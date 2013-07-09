faed8ddbeff00243af4e018a9112575811477b03
Georg Walther <georgwalther@gmail.com>
2013-07-09 10:49:17 +0100

    streamline doi and title fetching for extension to more journals

--- pubpeer_fetcher.js
+++ pubpeer_fetcher.js
@@ -1,3 +1,37 @@
+function get_title_element(hostname){
+	var title = null;
+	console.log("get_title_element hostname: "+hostname);
+	
+	if (hostname.indexOf("cell.com") != -1){
+		console.log("get_title_element cell.com detected");
+		title = document.getElementById('top_title');
+		if(!title){
+			title = document.getElementById('article_title');
+		}
+	}
+	else if (hostname.indexOf("peerj.com") != -1){
+		var h1 = document.getElementsByTagName('h1');
+		for(var i=0; i<h1.length; i++){
+				if(h1[i].className == "article-title"){
+						title = h1[i];
+				}
+		}
+	}
+	else if (hostname.indexOf("plos") != -1){
+		title = document.getElementsByTagName('h1')[0];
+	}
+	else if (hostname.indexOf("elifesciences") != -1){
+		var h1 = document.getElementsByTagName('h1');
+		for(var i=0; i<h1.length; i++){
+				if(h1[i].className == "page-title"){
+						console.log(h1[i]);
+						title = h1[i];				
+				}
+		}
+	}
+
+	return title;
+}
 
 var doi = null;
 var title = null;
@@ -5,72 +39,17 @@ var title = null;
 var hostname = window.location.hostname;
 console.log("hostname: "+hostname);
 
-if( hostname.indexOf("cell.com") != -1 ){
-	var meta = document.getElementsByTagName('meta');
+// we assume that DOI's are always in a <meta name="citation_doi"> tag
+var meta = document.getElementsByTagName('meta');
 
-	for(var i=0; i<meta.length; i++){
-			if(meta[i].name == "citation_doi"){
-					doi = meta[i].content;
-			}
-	}
-
-	title = document.getElementById('top_title');
-	if(!title){
-		title = document.getElementById('article_title');
-	}
-}
-else if( hostname.indexOf("peerj.com") != -1 ){
-		var meta = document.getElementsByTagName('meta');
-		var h1 = document.getElementsByTagName('h1');
-
-		for(var i=0; i<meta.length; i++){
-				if(meta[i].name == "citation_doi"){
-						doi = meta[i].content;
-				}
-		}
-
-		for(var i=0; i<h1.length; i++){
-				if(h1[i].className == "article-title"){
-						title = h1[i];
-				}
+for(var i=0; i<meta.length; i++){
+		if(meta[i].name == "citation_doi"){
+				doi = meta[i].content;
 		}
 }
-else if( hostname.indexOf("plos") != -1 ){
-		var meta = document.getElementsByTagName('meta');
-		var h1 = document.getElementsByTagName('h1');
+console.log("doi: "+doi);
 
-		title = document.getElementsByTagName('h1')[0];
-
-		for(var i=0; i<meta.length; i++){
-				if(meta[i].name == "citation_doi"){
-						doi = meta[i].content;
-				}
-		}
-}
-else if( hostname.indexOf("elifesciences") != -1 ){
-		var meta = document.getElementsByTagName('meta');
-		var h1 = document.getElementsByTagName('h1');
-
-		for(var i=0; i<meta.length; i++){
-				if(meta[i].name == "citation_doi"){
-						doi = meta[i].content;
-				}
-		}
-
-		for(var i=0; i<h1.length; i++){
-				if(h1[i].className == "page-title"){
-						console.log(h1[i]);
-						title = h1[i];				}
-		}
-}
-
-console.log("doi: "+doi+"; title: "+title);
-
-//title = h1[0].content;
-//alert(h1[0].textContent);
-
-//h1[0].style = "font-weight: bold";
-
+// get title element
 if(doi){
     var address = "http://api.pubpeer.com/v1/publications/"+doi+"?idType=doi&devkey=hack4ac";
     $.ajax(address
