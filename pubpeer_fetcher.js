82770364170073c175359b9ee1a5f57acfaecd57
Georg Walther <georgwalther@gmail.com>
2013-07-11 12:15:00 +0100

    add PubMed and arXiv support

--- pubpeer_fetcher.js
+++ pubpeer_fetcher.js
@@ -29,11 +29,24 @@ function get_title_element(hostname){
 				}
 		}
 	}
+	else if (hostname.indexOf("ncbi.nlm.nih.gov") != -1){
+		title = document.getElementsByTagName('h1')[1];
+	}
+	else if (hostname.indexOf("arxiv.org") != -1) {
+		var h1 = document.getElementsByTagName('h1');
+		for(var i=0; i<h1.length; i++){
+				if(h1[i].className == "title"){
+						title = h1[i];				
+				}
+		}
+	}
 
 	return title;
 }
 
 var doi = null;
+var pmid = null;
+var arxvi = null;
 var title = null;
 
 var hostname = window.location.hostname;
@@ -49,10 +62,28 @@ for(var i=0; i<meta.length; i++){
 }
 console.log("doi: "+doi);
 
+if (!doi) {
+	var dd = document.getElementsByTagName('dd');
+	try{
+		pmid = dd[0].innerHTML;
+	} catch(err) {
+		
+	}
+}
+console.log("pmid: "+pmid);
+
+if(!doi && !pmid){
+	var meta = document.getElementsByName('citation_arxiv_id')[0];
+	console.log("meta element found: "+meta.content);
+	arxiv = meta.content;
+	console.log("arxiv: "+arxiv);
+}
+
 // get title element
-if(doi){
+if(doi || pmid || arxiv){
 	title = get_title_element(hostname);
-	console.log("title element received from get_title_element: "+title);
+	console.log("Title element received from get_title_element: "+title);
+	console.log("Value of title element received: "+title.innerHTML);
 }
 
 // if doi and title element found, search for PubPeer content and alter title element
@@ -69,7 +100,17 @@ if(title){
 	$(title).css("cursor", "pointer");
 	$(title).css("text-decoration","underline");
 
-    var address = "http://api.pubpeer.com/v1/publications/"+doi+"?idType=doi&devkey=hack4ac";
+	var address = null;
+	if(doi){
+		address = "http://api.pubpeer.com/v1/publications/"+doi+"?idType=doi&devkey=hack4ac";
+	}
+	else if(pmid){
+		address = "http://api.pubpeer.com/v1/publications/"+pmid+"?idType=pubmed&devkey=hack4ac";
+	}
+	else if(arxiv){
+		address = "http://api.pubpeer.com/v1/publications/"+arxiv+"?idType=arxivd&devkey=hack4ac";
+	}
+	console.log("Address for PubPeer query: "+address);
     $.ajax(address
           ).done(function(data) {
               var json = $.parseJSON(data);
