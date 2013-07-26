d61f72adb3994c5eab0d870c55a97a824979196f
Georg Walther <georgwalther@gmail.com>
2013-07-26 23:24:32 +0100

    streamline identifier extraction

--- pubpeer_fetcher.js
+++ pubpeer_fetcher.js
@@ -17,44 +17,64 @@ function get_title_element(hostname){
 	return title;
 }
 
-var doi = null;
-var pmid = null;
-var arxiv = null;
-var title = null;
+function Identifier () {
+    this.id = null;
+    this.type = null;
+
+    var doi = null;
+    var pmid = null;
+    var arxiv = null;
+
+    // we assume that DOI's are always in a <meta name="citation_doi"> tag
+    var meta = document.getElementsByTagName('meta');
+    
+    for(var i=0; i<meta.length; i++){
+		if(meta[i].name == "citation_doi"){
+			doi = meta[i].content;
+		}
+    }
+    
+    if(doi){
+        this.id = doi;
+        this.type = "doi";
+        return this;
+    }
+
+    if (!doi) {
+	    var dd = document.getElementsByTagName('dd');
+	    try{
+		    pmid = dd[0].innerHTML;
+	    } catch(err) {
+
+	    }
+
+        if(pmid){
+            this.id = pmid;
+            this.type = "pmid";
+            return this;
+        }
+    }
+
+    if(!doi && !pmid){
+	    var meta = document.getElementsByName('citation_arxiv_id')[0];
+	    arxiv = meta.content;
+	    
+        if(arxiv){
+            this.id = arxiv;
+            this.type = "arxiv";
+            return this;
+        }
+    }
+
+    return null;
+}
+
 
 var hostname = window.location.hostname;
-console.log("hostname: "+hostname);
 
-// we assume that DOI's are always in a <meta name="citation_doi"> tag
-var meta = document.getElementsByTagName('meta');
+var identifier = new Identifier();
 
-for(var i=0; i<meta.length; i++){
-		if(meta[i].name == "citation_doi"){
-				doi = meta[i].content;
-		}
-}
-console.log("doi: "+doi);
-
-if (!doi) {
-	var dd = document.getElementsByTagName('dd');
-	try{
-		pmid = dd[0].innerHTML;
-	} catch(err) {
-		
-	}
-}
-console.log("pmid: "+pmid);
-
-if(!doi && !pmid){
-	var meta = document.getElementsByName('citation_arxiv_id')[0];
-	console.log("meta element found: "+meta.content);
-	arxiv = meta.content;
-	console.log("arxiv: "+arxiv);
-}
-
-
-// get title element
-if(doi || pmid || arxiv)
+if(identifier)
     title = get_title_element(hostname);
 
 // if doi and title element found, search for PubPeer content and alter title element
@@ -68,17 +88,9 @@ if(title){
 	title.style.color = "#fff";
 	$(title).css("cursor", "pointer");
 
-	var address = null;
-	if(doi){
-		address = "http://api.pubpeer.com/v1/publications/"+doi+"?idType=doi&devkey=hack4ac";
-	}
-	else if(pmid){
-		address = "http://api.pubpeer.com/v1/publications/"+pmid+"?idType=pubmed&devkey=hack4ac";
-	}
-	else if(arxiv){
-		address = "http://api.pubpeer.com/v1/publications/"+arxiv+"?idType=arxivd&devkey=hack4ac";
-	}
-	console.log("Address for PubPeer query: "+address);
+	var address = "http://api.pubpeer.com/v1/publications/"+identifier.id+"?idType="+identifier.type+"&devkey=hack4ac";    
+	console.log("articlEnhancer: Address for PubPeer query: "+address);
+
     $.ajax(address
           ).done(function(data) {
 			  var json = $.parseJSON(data);
